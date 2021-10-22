843e065ec33a438111e7f954f34d8dba1b8581d6
ThisisMiri <90226948+ThisisMiri@users.noreply.github.com>
2021-10-22 14:25:00 +0900

    211022 과제입니다 :)

---
+++ 211022/js-start/js/main.js
@@ -0,0 +1,306 @@
+//comic 박스 생성 = 변수 선언
+//var comic;
+
+//코믹박스 안에 원피스 데이터 할당한 상태 -> 변수 초기화
+//comic = "원피스";
+
+//console.log(comic);
+
+
+
+//변수선언과 동시에 초기화 진행
+//var apple = "사과";
+
+
+//변수동시선언
+/*
+var a;
+var b;
+var c;
+*/
+
+//var a,b,c;
+
+
+//변수동시초기화
+
+/*
+var d = 10;
+var e = 20;
+var f = 30;
+*/
+
+
+//var d=10, e = 20, f = 30;
+
+
+
+
+//모든 변수는 선언과 동시에 undefined 자동 할당.
+//var computer;
+
+//console.log(computer);
+
+
+
+/*
+var house;
+
+house = "집";
+console.log(house);
+
+
+house = "아파트";
+console.log(house);
+*/
+
+
+
+/*
+var house;
+console.log(house);
+
+//undefined -> 집
+house="집"
+console.log(house);
+*/
+
+
+
+
+/*
+변수명 작성요령
+
+캐멀(Camel) 케이스 <- 주로 사용하는 케이스.
+var mathScoreStudent = 10;
+
+스네이크 케이스
+var math_score_student = 10;
+*/
+
+/* 변수명 지을 때 주의점
+1. var math-score-student = 100 ; (x)
+2. [특수부호] var _math; var $math; _, $만 사용 가능
+3. var apple10;
+   var 10apple; 숫자가 먼저 나오는 것도 X
+4. 변수명 최대한 자세히 작성.
+*/
+
+
+
+
+//데이터 타입 = 데이터의 종류
+/*1. 문자열 (String) : 주로 검정 텍스트로 출력.
+    var msg1 = "Hello World";
+    var msg2 = 'Welcome';
+
+    console.log(msg1);
+    console.log(msg2);
+
+    ex) she is girl / she's girl
+    var msg4 = "she is girl";
+    var msg5 = 'she\'s girl';
+
+    console.log(msg4);
+    console.log(msg5);
+*/
+
+/*2. 숫자 (Number) : 파랑 텍스트로 출력
+    var num1 = 10;
+    var num2 = -10;
+    var num3 = 3.14;
+    var num4 = -3.14;
+
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+
+    console.log(num1 + num2);
+    console.log(num1 + num4)
+
+
+    //연산자
+    var a = 20;
+    var b = 10;
+
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+    console.log(a % b);
+
+
+    //문자열
+    var str1 = "20";
+    var str2 = "10";
+
+    console.log(str1 + str2);
+    console.log(str1 - str2);
+    console.log(str1 * str2);
+    console.log(str1 / str2);
+    console.log(str1 % str2);
+
+    var firstName = "Miri"
+    var lastName = "Kim";
+
+    console.log(firstName + "     " + lastName);
+
+    var str11 = "현재 시간은 ";
+    var time = 10;
+    var str12 = "시 입니다.";
+
+    console.log(str11 + time + str12);
+
+
+
+
+ // ++, -- 연산자
+    var num10 = 10;
+    console.log(--num10);
+    console.log(--num10);
+
+    console.log(++num10);
+    console.log(++num10);
+
+
+ // +=. -=, *=, /=, %= 연산자
+    var num20 = 20;
+
+    //num20 = num20 + 10;
+    num20 += 10;
+
+    console.log(num20);
+
+
+// 비교연산자 >, <, ==, ===, <=, >=, !=, !==
+    var a = 10;
+    var b = 20;
+    var c = 30;
+
+    console.log(a > b);
+    console.log(a < b);
+    console.log(a >= b);
+    console.log(a <= b);
+
+    console.log(a == b);
+    console.log(a === b);
+
+    console.log(a != b);
+    console.log(a !== b);
+
+
+    var num10 = 10;
+    var num20 = 20;
+
+    var str10 = "10"
+    var str20 = "20"
+
+    console.log(num10 === str10);
+   
+    console.log(num20 !== str20);
+    
+// 논리 연산자 : And (&&), Or(||)
+    var num30 = 30;
+    var num40 = 40;
+    var num50 = 50;
+
+    console.log(num30 === num40 && num30 === num50 && num40 !== num50);
+    console.log(num30 === num40 || num30 === num50 || num40 > num50);
+*/
+
+
+
+
+//3. 논리 (Boolean) : 
+
+    console.log(true);
+    console.log(false);
+    console.log(1 === 2);
+    console.log(10 < 20);
+    console.log(10 === 10 || 20 === 30);
+
+    var bool = true;
+    bool = false;
+
+
+
+
+
+
+
+
+
+//4. Undefined
+//5. Null
+//-> 원시타입
+
+//6. 배열 (Array)
+//7. 함수 (Function)
+//8. 객체 (Object)
+//-> 참조타입
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
